d01ad148d3e1553276d776f559fe9f3f8b685f3d
Ruchi Pundora <ruchipundora68@gmail.com>
2021-09-26 13:41:26 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Center(
+            child: Text('Roll the Dice'),
+          ),
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+  int rightDiceNumber =4;
+
+  void leftDiceFaceChange(){
+    setState(() {
+      leftDiceNumber= Random().nextInt(6)+1;
+    },);
+  }
+
+  void rightDiceFaceChange(){
+    setState(() {
+      rightDiceNumber= Random().nextInt(6)+1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                leftDiceFaceChange();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                rightDiceFaceChange();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
